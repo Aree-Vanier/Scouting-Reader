c7fe4dc807b095ccce45238a4ba16cc87b25b066
Aree Vanier <gregory@ajayinkingston.com>
2018-10-16 11:56:06 -0400

    Added code to read robot data

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const {app, BrowserWindow, ipcMain} = require("electron");
 const fs = require("fs");
 
+const PATH = "C:\\Users\\kellygr\\Downloads\\Ontario Champs Tech\\Ontario Champs Tech\\";
+
 var windows = {
 	"index":null,
 	"auto":null
@@ -79,8 +81,8 @@ ipcMain.on("show-alliance", function(e, teams){
 
 
 
-ipcMain.on("get-data", function(e, team){
-	fs.readFile("C:\\Users\\kellygr\\Downloads\\Ontario Champs Tech\\Ontario Champs Tech\\2708.csv", 'utf-8', (err, data) => {
+ipcMain.on("get-data", function(event, team){
+	fs.readFile(PATH+"2708"+".csv", 'utf-8', (err, data) => {
 		console.log(err);
 		console.log(data);
 		
@@ -88,12 +90,22 @@ ipcMain.on("get-data", function(e, team){
 		var teleStart = 10;
 		var endgameStart = 19;
 		var otherStart = 21;
-		var lines = input.split("\n");
+		var lines = data.split("\n");
 		
 		var header = lines[0].split(",");
 		var out = {
-		  "robot":lines[1].split(",")[0],
+		  "general":{},
+		  "averages":{},
 		  "matches":[]};
+		
+		out["general"]["number"]=lines[1].split(",")[0];
+		out["general"]["name"]="team name here";
+		out["general"]["rank"]=1;
+		out["general"]["rpa"]=4;
+		out["general"]["opr"]=100;
+		out["general"]["dpr"]=100;
+		out["general"]["matchCount"] = lines.length-1;
+		
 		for(var m=1; m<lines.length; m++){
 		  data=lines[m].split(",");
 		  console.log(m+"\t"+data[1])
@@ -128,8 +140,74 @@ ipcMain.on("get-data", function(e, team){
 		  out["matches"].push(match);
 		}
 
+		var maxScale = 0;
+		var maxSwitch = 0;
+		var maxVault = 0;
+		var maxTotal = 0;
+		var sumScale = 0;
+		var sumSwitch = 0;
+		var sumVault = 0;
+		var sumTotal = 0;
+		var sumScaleMiss = 0;
+		var sumSwitchMiss = 0;
+		var sumVaultMiss = 0;
+
+
+		var matchCount = out["general"]["matchCount"]
+
+		var matches = out.matches;
+
+		for(var i = 0; i<matches.length; i++){
+		  let scale = parseInt(matches[i]["tele"]["Scale Cubes"])
+		  let sw = parseInt(matches[i]["tele"]["Own Switch Cubes"])
+			  +parseInt(matches[i]["tele"]["Other Switch Cubes"])
+		  let vault = parseInt(matches[i]["tele"]["Vault Cubes"])
+		  let total = parseInt(scale+sw+vault)
+
+		  console.log(scale+"\t"+sw+"\t"+vault)
+
+		  let scaleMiss = parseInt(matches[i]["tele"]["Scale Miss"])
+		  let swMiss = parseInt(matches[i]["tele"]["Own Switch Miss"])
+			  +parseInt(matches[i]["tele"]["Other Switch Miss"])
+		  let vaultMiss = parseInt(matches[i]["tele"]["Vault Miss"])
+
+		  maxScale = scale>maxScale?scale:maxScale;
+		  maxSwitch = sw>maxSwitch?sw:maxSwitch;
+		  maxVault = vault>maxVault?vault:maxVault;
+		  maxTotal = total>maxTotal?total:maxTotal;
+
+		  sumScale += scale;
+		  sumSwitch += sw;
+		  sumVault += vault;
+		  sumTotal += total;
+		  
+		  sumScaleMiss += scaleMiss;
+		  sumSwitchMiss += swMiss;
+		  sumVaultMiss += vaultMiss;
+
+		}
+		
+		var averages = {};
+
+		averages["Max Scale"] = maxScale;
+		averages["Average Scale"] = sumScale/matchCount;
+		averages["Scale Accuracy"] = sumScale/(sumScale+sumScaleMiss)
+
+		averages["Max Switch"] = maxSwitch;
+		averages["Average Switch"] = sumSwitch/matchCount;
+		averages["Switch Accuracy"] = sumSwitch/(sumSwitch+sumSwitchMiss)
+
+		averages["Max Vault"] = maxVault;
+		averages["Average Vault"] = sumVault/matchCount;
+		averages["Vault Accuracy"] = sumVault/(sumVault+sumVaultMiss)
+
+		averages["Max Total"] = maxTotal;
+		averages["Average Total"] = sumTotal/matchCount;
+
+		out["averages"] = averages;
+				
 		console.log(out);
-		event.sender.set("set-data", out);
+		event.sender.send("set-data", out);
 	});
 })
 
